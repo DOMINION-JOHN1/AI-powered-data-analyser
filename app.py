9e8476f92a5953f0c08c337bf02e934cd7d90132
DOMINION JOHN <119686378+DOMINION-JOHN1@users.noreply.github.com>
2024-07-30 13:30:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ llm = OpenAI(api_token=openai_api_key)
 
 st.set_page_config(layout='wide')
 st.title("Genie🤓")
+st.image("download(1).jpg", width=800)
 st.sidebar.image("download.jpg", width=400)
 # Catchy description
 st.sidebar.markdown("""
